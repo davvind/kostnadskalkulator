2a69b6bdd3c32e44f91a3dc5dd0180595f13ac87
davvind <oiesvold@gmail.com>
2026-04-10 16:44:14 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'kostnad-v1';
+const FILES = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
